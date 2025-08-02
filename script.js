ca373f6893b6a09a9080262c1d24af4a6d8c79df
Ashish Verma <vermaashish510@gmail.com>
2025-08-03 01:58:26 +0530

    Concept of defer binding in script

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*comenting addEvcentListener to avoid multiple bindings
+If you want to use it, uncomment the line below
+if we are not deferring the script, we can use DOMContentLoaded
+to ensure the DOM is fully loaded before running the script*/
 // document.addEventListener("DOMContentLoaded", () => {
 const button = document.getElementById("clickHereButton");
 const output = document.getElementById("output");
